df10c5e7f9db9305dbccc923fe10bf5901157e46
Błażej Wróbel <blazej.wrobel@microsoft.wsei.edu.pl>
2020-03-02 17:00:31 +0100

    hehe obiektowo

--- NewProject/Program.cs
+++ NewProject/Program.cs
@@ -3,51 +3,105 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace NewProject
 {
-    class Program
+    public class Program
     {
-        const int men_em = 65;
-        const int wom_em = 60;
+        public static readonly int[] eme_age = { 60, 65 };
+        public static readonly string[] menu_plec = {"Kobieta", "Mężczyzna"};
         static void Main(string[] args)
         {
             Console.WriteLine("Aplikacja Emerytura");
-            Console.Write("Podaj imie: ");
-            string imie = Console.ReadLine().Trim();
-            Console.Write("Podaj nazwisko: ");
-            string nazwisko = Console.ReadLine().Trim();
+
+            firstname:
+            string imie = GetData("imie");
+            if ((InputCheck(imie)) == false)
+            {
+                goto firstname;
+            }
+
+            lastname:
+            string nazwisko = GetData("nazwisko");
+            if ((InputCheck(nazwisko)) == false)
+            {
+                goto lastname;
+            }
+
             Console.WriteLine($"Witaj {imie} {nazwisko}");
             Console.Write("Podaj wiek: ");
             int wiek = int.Parse(Console.ReadLine());
             Console.WriteLine("Wybierz płeć spośród dostępnych: ");
-            Console.WriteLine("[0] KOBIETA ");
-            Console.WriteLine("[1] MĘŻCZYZNA ");
-            int plec = int.Parse(Console.ReadLine());
-            switch (plec)
+            for(int i = 0; i < menu_plec.Length; i++)
             {
-                case 0:
-                    if (wiek >= wom_em)
-                    {
-                        Console.WriteLine("Jesteś EMERYTEM");
-                        break;
-                    }
-                    else
-                    {
-                        Console.WriteLine($"Osiągniesz wiek emerytalny za {wom_em - wiek} lat");
-                        break;
-                    }
-                case 1:
-                    if (wiek >= men_em)
-                    {
-                        Console.WriteLine("Jesteś EMERYTEM");
-                        break;
-                    }
-                    else
-                    {
-                        Console.WriteLine($"Osiągniesz wiek emerytalny za {men_em - wiek} lat");
-                        break;
-                    }
+                Console.WriteLine($"[{i}] {menu_plec[i]} ");
+            }
+            int plec = int.Parse(Console.ReadLine());
+            int do_em = eme_age[plec] - wiek;
+            if (do_em > 0)
+            {
+                Console.WriteLine($"Do emerytury pozostało ci {do_em}");
+            }
+            else if(do_em == 0)
+            {
+                Console.WriteLine("Na emeryturę przejdziesz w tym roku");
+            }
+            else
+            {
+                Console.WriteLine("Jesteś emerytem");
+            }
+        }
+        public static bool InputCheck(string value)
+        {
+            if ((String.IsNullOrEmpty(value)) == true || (Regex.IsMatch(value, "[0-9]")) == true)
+            {
+                Console.WriteLine("Wprowadzono błędne dane, spróbuj jeszcze raz :) ");
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        public static string GetData(string value)
+        {
+            Console.Write($"Podaj {value}: ");
+            var temp = Console.ReadLine().Trim();
+            return temp;
+        }
+    }
+    class User
+    {
+        string imie = "";
+        string nazwisko = "";
+        int wiek = 0;
+        
+        static void Main(string[] args)
+        {
+            User myUser = new User();
+            myUser.imie = GetData("imie");
+            myUser.nazwisko = GetData("nazwisko");
+            myUser.wiek = GetAge();
+        }
+        static string GetData(string value)
+        {
+            while (true)
+            {
+                string temp = Program.GetData($"{value}");
+                if ((Program.InputCheck(temp)) == true)
+                {
+                    return temp;
+                }
+            }
+        }
+        static int GetAge()
+        {
+            while (true)
+            {
+                string temp = Program.GetData("wiek");
+                int wiek = int.Parse(temp);
+                return wiek;
             }
         }
     }
