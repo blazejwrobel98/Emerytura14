fa6f34ff2a481e6a152c098afe952249027a1ef4
Błażej Wróbel <blazej.wrobel@microsoft.wsei.edu.pl>
2020-03-03 10:39:03 +0100

    Finished

--- NewProject/Program.cs
+++ NewProject/Program.cs
@@ -7,34 +7,36 @@ namespace NewProject
     {
         public static readonly int[] eme_age = { 60, 65 };
         public static readonly string[] menu_plec = { "Kobieta", "Mężczyzna" };
-        static void Main(string[] args)
+        public static User user_data;
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Aplikacja Emerytura");
-
-            GlobalMenu.GenerateMenu();
-            User user_data = new User();
-
-            Console.WriteLine($"Witaj {user_data.imie} {user_data.nazwisko}!\nMasz {user_data.wiek} lat");
-
-            int do_em = eme_age[user_data.plec] - user_data.wiek;
-            if (do_em > 0)
+            Console.WriteLine(" Aplikacja Emerytura");
+            while (true)
             {
-                Console.WriteLine($"Do emerytury pozostało ci {do_em}");
-            }
-            else if (do_em == 0)
-            {
-                Console.WriteLine("Na emeryturę przejdziesz w tym roku");
-            }
-            else
-            {
-                Console.WriteLine("Jesteś emerytem");
+                Console.Clear();
+                int value = GlobalMenu.GenerateMenu();
+                if (value != 0)
+                {
+                    Console.Clear();
+                    GlobalMenu.Option(value);
+                }
+                else
+                {
+                    break;
+                }
             }
         }
         public static bool InputCheck(string value)
         {
+            Console.Clear();
+            if(value.Trim().ToUpper() == "DUPA")
+            {
+                Console.WriteLine(" Witam Pana :) ");
+                return false;
+            }
             if ((String.IsNullOrEmpty(value)) == true || (Regex.IsMatch(value, "[0-9]")) == true)
             {
-                Console.WriteLine("Wprowadzono błędne dane, spróbuj jeszcze raz :) ");
+                Console.WriteLine(" Wprowadzono błędne dane, spróbuj jeszcze raz :) ");
                 return false;
             }
             else
@@ -44,9 +46,15 @@ namespace NewProject
         }
         public static bool IntCheck(string value)
         {
+            Console.Clear();
+            if (value.Trim().ToUpper() == "DUPA")
+            {
+                Console.WriteLine(" Witam Pana :) ");
+                return false;
+            }
             if ((String.IsNullOrEmpty(value)) == true || (Regex.IsMatch(value, "[^0-9]")) == true)
             {
-                Console.WriteLine("Wprowadzono błędne dane, spróbuj jeszcze raz :) ");
+                Console.WriteLine(" Wprowadzono błędne dane, spróbuj jeszcze raz :) ");
                 return false;
             }
             else
@@ -56,9 +64,14 @@ namespace NewProject
         }
         public static string GetData(string value)
         {
-            Console.Write($"Podaj {value}: ");
+            Console.Write($" Podaj {value}: ");
             var temp = Console.ReadLine().Trim();
             return temp;
+        }
+        public static void Pause()
+        {
+            Console.WriteLine($"\n Wciśnij dowolny przycisk aby kontynuować...");
+            Console.ReadKey();
         }
     }
 
@@ -98,10 +111,10 @@ namespace NewProject
             while (true)
             {
             start:
-                Console.WriteLine("Wybierz płeć spośród dostępnych: ");
+                Console.WriteLine(" Wybierz płeć spośród dostępnych: ");
                 for (int i = 0; i < Program.menu_plec.Length; i++)
                 {
-                    Console.WriteLine($"[{i}] {Program.menu_plec[i]} ");
+                    Console.WriteLine($" [{i}] {Program.menu_plec[i]} ");
                 }
                 string temp = Console.ReadLine();
                 if ((Program.IntCheck(temp)) == true)
@@ -113,7 +126,7 @@ namespace NewProject
                     }
                     else
                     {
-                        Console.WriteLine("Wprowadzono błędne dane, spróbuj jeszcze raz :) ");
+                        Console.WriteLine(" Wprowadzono błędne dane, spróbuj jeszcze raz :) ");
                         goto start;
                     }
 
@@ -125,33 +138,94 @@ namespace NewProject
     public class GlobalMenu
     {
         public static readonly string[] Global_menu_elements = { "WYJŚCIE", "Wprowadź DANE", "Wyświetl DANE", "Skasuj DANE", "Sprawdź kiedy EMERYTURA", "Wyświetl USTAWIENIA" };
-        public static void GenerateMenu()
+        public static int GenerateMenu()
         {
-            for (int i = 0; i < Global_menu_elements.Length; i++){
-                Console.WriteLine($"[{i}] {Global_menu_elements[i]} ");
-            }
-            var temp = GetInput();
-        }
-        public static int GetInput()
-        {
-            getinput:
-            string temp = Console.ReadLine();
             while (true)
             {
+            start:
+                for (int i = 0; i < Global_menu_elements.Length; i++)
+                {
+                    Console.WriteLine($" [{i}] {Global_menu_elements[i]} ");
+                }
+                string temp = Console.ReadLine();
+
                 if ((Program.IntCheck(temp)) == true)
                 {
                     int option = int.Parse(temp);
-                    if ((option < Program.menu_plec.Length) == true)
+                    if ((option < Global_menu_elements.Length) == true)
                     {
                         return option;
                     }
                     else
                     {
-                        Console.WriteLine("Wprowadzono błędne dane, spróbuj jeszcze raz :) ");
-                        goto getinput;
+                        Console.Clear();
+                        Console.WriteLine(" Wprowadzono błędne dane, spróbuj jeszcze raz :) ");
+                        goto start;
                     }
 
                 }
+            }
+        }
+        public static void Option(int value)
+        {
+            switch (value)
+            {
+                case 1:
+                    Program.user_data = new User();
+                    Program.Pause();
+                    break;
+                case 2:
+                    if (Program.user_data == null)
+                    {
+                        Console.WriteLine("\n Brak przypisanych wartości!!!\n");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"\n Imię: {Program.user_data.imie} \n Nazwisko: {Program.user_data.nazwisko}\n Wiek: {Program.user_data.wiek}");
+                    }
+                    Program.Pause();
+                    break;
+                case 3:
+                    Program.user_data = null;
+                    Console.WriteLine("\n Dane SKASOWANE!\n");
+                    Program.Pause();
+                    break;
+                case 4:
+                    if (Program.user_data == null)
+                    {
+                        Console.WriteLine("\n Brak przypisanych wartości!!!\n");
+                    }
+                    else
+                    {
+                        int do_em = Program.eme_age[Program.user_data.plec] - Program.user_data.wiek;
+                        if (do_em > 0)
+                        {
+                            Console.WriteLine($"\n Do emerytury pozostało ci {do_em}\n");
+                        }
+                        else if (do_em == 0)
+                        {
+                            Console.WriteLine("\n Na emeryturę przejdziesz w tym roku\n");
+                        }
+                        else
+                        {
+                            Console.WriteLine("\n Jesteś emerytem\n");
+                        }
+                    }
+                    Program.Pause();
+                    break;
+                case 5:
+                    Console.WriteLine("\nLista dostępnych płci: ");
+                    foreach (var item in Program.menu_plec)
+                    {
+                        Console.WriteLine($" {item.ToString()}");
+                    }
+                    Console.WriteLine("\nWiek emerytalny dla kobiet i mężczyzn: ");
+                    for (int i = 0; i < Program.menu_plec.Length; i++)
+                    {
+                        Console.WriteLine($" {Program.menu_plec[i]} : {Program.eme_age[i]}");
+                    }
+                    Program.Pause();
+                    break;
             }
         }
     }
